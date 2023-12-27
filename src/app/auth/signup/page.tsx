83dcd966d03d6aa53f809ca0f2c2db5a74b8cab6
Nitin Jaswal <62323476+walkerthecreator@users.noreply.github.com>
2023-12-28 00:18:33 +0530

    refactoreded

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -5,7 +5,7 @@ import SignInWithGoogle from "@/components/SignInWithGoogle";
 export default function Signup(){
     return (
         <>
-        <div className="w-screen p-10 drop-shadow-lg">
+        <div className="w-screen p-10 drop-shadow-lg mt-10">
         <div className="w-96 border border-zinc-800 bg-white text-black p-4 px-4 flex flex-col rounded-md mx-auto">
 
             <h1 className="text-3xl font-satoshi font-semibold">sign up</h1>
@@ -13,7 +13,7 @@ export default function Signup(){
 
             <input type="text" placeholder="Enter email" className="p-1 border rounded-md" />
             <input type="text" placeholder="Enter password" className="p-1 border rounded-md my-2" />
-            <button className="bg-black border rounded-md text-white font-semibold p-1">Sign up</button>
+            <button className="bg-black font-satoshi border rounded-md text-white font-semibold p-1">Sign up</button>
 
             <span className="text-zinc-500 text-xs text-center my-2">or</span>
 
