f760fc0dacf37e8a5eb6bc24eb3d4f2ab8ef5feb
walkerthecreator <nitinjaswal2616@gmail.com>
2024-04-01 19:04:42 +0530

    fixed about Es-lint page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About(){
     return <div id="about" className="bg-slate-00 text-white min-h-screen overflow-auto">
         <div className="mt-52 w-4/5 mx-auto text-center">
             <h1 className="text-center text-7xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-blue-400 via-zinc-50 to-zinc-100">Read It Before You Rent</h1>
-            <p className="text-2xl font-semibold mt-2">The Honest Tenant's Voice - Review Your Rental</p>
+            <p className="text-2xl font-semibold mt-2">The Honest Tenant&apos;s Voice - Review Your Rental</p>
         </div>
 
         <div className="flex w-3/4 mx-auto justify-center gap-5 items-center mt-14">
@@ -14,7 +14,7 @@ export default function About(){
                 </button>
 
                 <button className="text-5xl font-semibold p-1 h-16 w-52 text-lime-700  border-lime-100 border-t-2 drop-shadow-md rounded-lg bg-gradient-to-b from-lime-200 to-lime-300 ring-1 ring-lime-300">
-                <span className="bg-gradient-to-b text-transparent from-lime-500 to-lime-700 bg-clip-text">PG's</span>
+                <span className="bg-gradient-to-b text-transparent from-lime-500 to-lime-700 bg-clip-text">PG&apos;s</span>
                 </button>
 
                 <button className="text-5xl font-semibold p-1 h-16 w-52  border-emerald-100 border-t-2 drop-shadow-md rounded-lg bg-gradient-to-b from-emerald-200 to-emerald-300 ring-1 ring-emerald-300">
